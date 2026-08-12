bb698eadb1e64127146c68061ea3421d10796480
Godnit <gema.123.9000@gmail.com>
2026-08-12 18:26:18 +0300

    Add Psychology Academy WebView activity

---
+++ psychology-academy/app/src/main/java/com/godnit/psychologyacademy/MainActivity.java
@@ -0,0 +1,35 @@
+package com.godnit.psychologyacademy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.graphics.Color;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.view.View;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+    private WebView web;
+    @Override public void onCreate(Bundle b) {
+        super.onCreate(b);
+        Window w=getWindow();
+        w.setStatusBarColor(Color.rgb(14,26,37));
+        w.setNavigationBarColor(Color.rgb(14,26,37));
+        web=new WebView(this);
+        web.setBackgroundColor(Color.rgb(14,26,37));
+        WebSettings s=web.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+        s.setBuiltInZoomControls(false);
+        s.setDisplayZoomControls(false);
+        s.setTextZoom(100);
+        web.setWebViewClient(new WebViewClient());
+        web.setOverScrollMode(View.OVER_SCROLL_NEVER);
+        setContentView(web);
+        web.loadUrl("file:///android_asset/index.html");
+    }
+    @Override public void onBackPressed(){ web.evaluateJavascript("window.appBack && window.appBack()", null); }
+}
