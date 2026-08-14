8a1062bd2e7f66863cd005b4d0abc8b333d8fe4f
Godnit <gema.123.9000@gmail.com>
2026-08-14 12:12:37 +0300

    Optimize SVG map rendering for mobile

---
+++ empireapp/src/main/assets/phase20-optimize.js
@@ -0,0 +1,20 @@
+(function(){
+'use strict';
+try{
+  const svg=document.getElementById('world');
+  if(!svg)return;
+  const paths=[...svg.querySelectorAll('.p2admin')];
+  if(paths.length>1){
+    const d=paths.map(p=>p.getAttribute('d')||'').filter(Boolean).join(' ');
+    if(d){
+      const merged=document.createElementNS('http://www.w3.org/2000/svg','path');
+      merged.setAttribute('class','p2admin p20AdminMerged');
+      merged.setAttribute('d',d);
+      paths[0].parentNode.insertBefore(merged,paths[0]);
+      paths.forEach(p=>p.remove());
+    }
+  }
+  const loading=document.getElementById('p2loading');
+  if(loading&&loading.classList.contains('hide'))loading.remove();
+}catch(_){ }
+})();
