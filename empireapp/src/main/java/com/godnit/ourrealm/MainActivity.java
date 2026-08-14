e4012a97b5ba2907b20142219aba6c699c6b6438
Godnit <gema.123.9000@gmail.com>
2026-08-14 12:11:01 +0300

    Load map state bridge before final controller

--- empireapp/src/main/java/com/godnit/ourrealm/MainActivity.java
+++ empireapp/src/main/java/com/godnit/ourrealm/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends Activity {
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
                 String js = "(function(){" +
-                        "var files=['map_countries.js','map_admin1.js','map_cities.js','map_yemen_admin.js','phase2-map.js','phase20-final-map.js'];" +
+                        "var files=['map_countries.js','map_admin1.js','map_cities.js','map_yemen_admin.js','phase2-map.js','phase20-context.js','phase20-final-map.js'];" +
                         "function next(i){if(i>=files.length)return;var s=document.createElement('script');" +
                         "s.src=files[i]+'?v=20';s.onload=function(){next(i+1)};" +
                         "s.onerror=function(){next(i+1)};document.body.appendChild(s);}next(0);" +
