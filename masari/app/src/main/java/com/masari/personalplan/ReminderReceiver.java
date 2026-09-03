5213c126092522e4ead4a2de1897dfbcd4275410
Godnit <gema.123.9000@gmail.com>
2026-09-03 16:32:21 +0300

    Open professional dashboard from reminders

--- masari/app/src/main/java/com/masari/personalplan/ReminderReceiver.java
+++ masari/app/src/main/java/com/masari/personalplan/ReminderReceiver.java
@@ -26,7 +26,8 @@ public class ReminderReceiver extends BroadcastReceiver {
             ch.setLightColor(Color.rgb(22,123,98));
             nm.createNotificationChannel(ch);
         }
-        Intent open = new Intent(context, MasariRewardsActivityV4.class);
+        Intent open = new Intent(context, ProfessionalDashboardActivity.class);
+        open.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         PendingIntent pi = PendingIntent.getActivity(context, id, open, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         android.app.Notification.Builder b = Build.VERSION.SDK_INT >= 26
                 ? new android.app.Notification.Builder(context, CHANNEL_ID)
