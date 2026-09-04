502340e10bd77c786179f448f335f566a1e50a78
Godnit <gema.123.9000@gmail.com>
2026-09-04 15:21:11 +0300

    Open new Masari pro dashboard from reminders

--- masari/app/src/main/java/com/masari/personalplan/ReminderReceiver.java
+++ masari/app/src/main/java/com/masari/personalplan/ReminderReceiver.java
@@ -23,10 +23,10 @@ public class ReminderReceiver extends BroadcastReceiver {
             NotificationChannel ch = new NotificationChannel(CHANNEL_ID, "تذكيرات مساري", NotificationManager.IMPORTANCE_DEFAULT);
             ch.setDescription("تذكيرات قبل المهام المهمة");
             ch.enableLights(true);
-            ch.setLightColor(Color.rgb(22,123,98));
+            ch.setLightColor(Color.rgb(24,142,78));
             nm.createNotificationChannel(ch);
         }
-        Intent open = new Intent(context, ProfessionalDashboardActivity.class);
+        Intent open = new Intent(context, MasariProActivity.class);
         open.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         PendingIntent pi = PendingIntent.getActivity(context, id, open, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         android.app.Notification.Builder b = Build.VERSION.SDK_INT >= 26
