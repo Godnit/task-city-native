7aca8bf2facc7f822972481c4dea9977b2eeb44b
Godnit <gema.123.9000@gmail.com>
2026-09-04 15:26:13 +0300

    Open MasariV10Activity from reminders

--- masari/app/src/main/java/com/masari/personalplan/ReminderReceiver.java
+++ masari/app/src/main/java/com/masari/personalplan/ReminderReceiver.java
@@ -26,7 +26,7 @@ public class ReminderReceiver extends BroadcastReceiver {
             ch.setLightColor(Color.rgb(24,142,78));
             nm.createNotificationChannel(ch);
         }
-        Intent open = new Intent(context, MasariProActivity.class);
+        Intent open = new Intent(context, MasariV10Activity.class);
         open.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         PendingIntent pi = PendingIntent.getActivity(context, id, open, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         android.app.Notification.Builder b = Build.VERSION.SDK_INT >= 26
